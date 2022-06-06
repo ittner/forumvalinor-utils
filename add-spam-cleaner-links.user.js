f0896987af715184c7b4be5caaca86425e2b33fe
Alexandre Erwin Ittner <alexandre@ittner.com.br>
2022-06-06 07:58:51 -0300

    Use a cleaner approach for the same effect

--- add-spam-cleaner-links.user.js
+++ add-spam-cleaner-links.user.js
@@ -33,35 +33,20 @@ https://www.valinor.com.br/forum/spam-cleaner/soikeobong88live.118313/?no_redire
         }
 
         let prefix = parts[0];
-        let rx = new RegExp('.+/admin\\.php\\?users/.+\\.[0-9]+/edit');
+        let rx = new RegExp('.+/admin\\.php\\?users/(.+)\\.([0-9]+)/edit');
 
         console.log("Adding Spam Cleaner links");
-       
+
         let links = document.getElementsByTagName("a");
         for (var i = 0; i < links.length; i++) {
-            if (rx.test(links[i].href)) {
-
-                // <span class="username " dir="auto" data-user-id="118313">soikeobong88live</span>
-                // <span class="dataList-hint" dir="auto">soikeobong88live@gmail.com</span>
-                let spans = links[i].getElementsByTagName("span");
-                var userid = 0;
-                var target = null;
-                for (var j = 0; j < spans.length; j++) {
-                    var tmp;
-                    if (tmp = parseInt(spans[j].getAttribute("data-user-id"))) {
-                        userid = tmp;
-                    }
-                    if (spans[j].classList.contains("dataList-hint")) {
-                        target = spans[j];
-                    }
-                }
-
-                if (userid > 0 && target != null) {
+            let m = links[i].href.match(rx);
+            if (m) {
+                let divs = links[i].getElementsByTagName("div");
+                if (divs.length > 0 && divs[0].classList.contains("dataList-mainRow")) {
                     var a = document.createElement("a");
-                    a.href = prefix + "/spam-cleaner/xx." + userid + "/?no_redirect=1";
+                    a.href = prefix + "/spam-cleaner/" + m[1] + "." + m[2] + "/?no_redirect=1";
                     a.innerText = "[spam clean]";
-                    target.appendChild(document.createTextNode(" "));
-                    target.appendChild(a);
+                    divs[0].appendChild(a);
                     console.log(a.href);
                 }
             }
